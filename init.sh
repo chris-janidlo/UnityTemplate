17724b8ef7bfa2de73edab3917511a7f688d46ef
Chris Janidlo <christopherjanidlo@gmail.com>
2022-10-30 12:11:04 -0500

    Initialize `git lfs`, if installed

--- init.sh
+++ init.sh
@@ -9,3 +9,10 @@ rm init.sh
 git add .
 git commit -m "Automated cleanup commit"
 git push
+
+
+if command -v git-lfs; then
+    git lfs install
+else
+    echo "UnityTemplate: git-lfs not initialized due to not being found in path. recommend installing the extension from https://git-lfs.github.com/"
+fi
